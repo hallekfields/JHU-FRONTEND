b636568c5270e7f4be09ff3e6c95ff8eee245615
Halle Fields <hallekfields@MacBook-Pro.local>
2022-12-14 01:08:52 -0500

    pushing final

--- assignment10/spec/menu.service.spec.js
+++ assignment10/spec/menu.service.spec.js
@@ -5,7 +5,7 @@ describe('menu service', function () {
     var ApiPath;
   
     beforeEach(function () {
-      module('restaurant');
+      module('common');
   
       inject(function ($injector) {
         menu = $injector.get('MenuService');
@@ -17,7 +17,7 @@ describe('menu service', function () {
     it('should return menu item', function() {
       $httpBackend.whenGET(ApiPath + '/menu_items/L/menu_items/5.json').respond(['description', 'name']);
       menu.getMenuItemByShortName('L6').then(function(response) {
-        expect(response.data).toEqual(['description', 'name']);
+        expect(response).toEqual(['description', 'name']);
       });
       $httpBackend.flush();
     });
